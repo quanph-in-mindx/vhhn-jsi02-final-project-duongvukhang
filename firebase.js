fb9636445193cb6c10c369ae260586597cf220b0
duongvukhang <duongvukhang1009@gmail.com>
2023-10-29 14:56:21 +0700

    hehe

---
+++ firebase.js
@@ -0,0 +1,46 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAHZ2KqzoRpcyazjTEcKxJWfAurAt5nfuI",
+    authDomain: "impact-bce0c.firebaseapp.com",
+    projectId: "impact-bce0c",
+    storageBucket: "impact-bce0c.appspot.com",
+    messagingSenderId: "189078421583",
+    appId: "1:189078421583:web:ffda33aa41790c49b02a3a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+const signinWithGoogle = ()=> {
+ return signInWithPopup(auth, provider)
+        .then((result) => {
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            localStorage.setItem("user" , JSON.stringify(user));
+            window.location.href = "./f.html";
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+        }).catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+
+}
+export { app, auth, signinWithGoogle };
