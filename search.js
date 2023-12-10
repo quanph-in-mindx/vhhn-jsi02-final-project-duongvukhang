2ed7d6ac34fac0d60e2e81a6dcc9fc88a6135ce2
Your Name <you@example.com>
2023-12-10 12:42:40 +0700

    ss67

--- search.js
+++ search.js
@@ -1,43 +1,42 @@
 $(document).ready(() => {
-    $("#searchForm").on('submit', (e) => {
-      e.preventDefault();
-      let searchText = $("#searchText").val();
-      getComics(searchText);
-    });
+  $("#searchForm").on('submit', (e) => {
+    e.preventDefault();
+    let searchText = $("#searchText").val();
+    getComics(searchText);
   });
-  
-  function getComics(searchText){
-    
-    // resquest cho id
-    axios.get(`https://comics-api.vercel.app/search?q=${searchText}&page=1`)
-      .then(function (response) {
-        console.log(response);
-        let comics = response.data.results;
-        
-        localStorage.setItem("comics",JSON.stringify(comics))
-        let output = '';
-        $.each(comics, (index, comic) => {
-          output+=`
+});
+
+function getComics(searchText) {
+
+  // resquest cho id
+  axios.get(`https://comics-api.vercel.app/search?q=${searchText}&page=1`)
+    .then(function (response) {
+      console.log(response);
+      let comics = response.data.results;
+
+      localStorage.setItem("comics", JSON.stringify(comics))
+      let output = '';
+      $.each(comics, (index, comic) => {
+        output += `
             <div class="col-md-3">
               <div class="well text-center">
-                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+                <img src="https://comics-api.vercel.app/search?q=${comic}&page=1">
                 <h5>${comic.title}</h5>
                 <a onclick="comicSelected('${index}')" class="btn btn-primary" href="#">Comic Details</a>
               </div>
             </div>
           `;
-        });
-        $('#comics').html(output);
-      })
-      .catch(function (error) {
-        console.log(error);
       });
-  }
-  
-  function comicSelected(comic){
-    // localStorage.setItem("movie",JSON.stringify(movie))
-    window.location ='detail.html?id='+comic;
-    return false;
-  }
-  
- 
+      $('#comics').html(output);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function comicSelected(comic) {
+  // localStorage.setItem("movie",JSON.stringify(movie))
+  window.location = 'detail.html?id=' + comic;
+  return false;
+}
+
