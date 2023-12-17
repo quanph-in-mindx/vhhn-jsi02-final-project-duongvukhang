6dd356441b4d6dc8cc1d97d947d913f813b3787b
Your Name <you@example.com>
2023-12-17 15:55:57 +0700

    test

--- require-auth.js
+++ require-auth.js
@@ -1,30 +1,30 @@
-let user =localStorage.getItem("user");
-if(user){
-    user = JSON.parse (localStorage.getItem("user"));
+let user = localStorage.getItem("user");
+if (user) {
+    user = JSON.parse(localStorage.getItem("user"));
 
-    document.addEventListener("DOMContentLoaded",()=> {
-        const nameEl =document.getElementById("name")
-        const emailEl =document.getElementById("email")
-        const avatarEl =document.getElementById("avatar")
+    document.addEventListener("DOMContentLoaded", () => {
+        const nameEl = document.getElementById("name")
+        const emailEl = document.getElementById("email")
+        const avatarEl = document.getElementById("avatar")
 
-        
-        nameEl.innerHTML =user.displayName;
-        emailEl.innerHTML=user.email;
-        avatarEl.src=user.photoURL;
+
+        nameEl.innerHTML = user.displayName;
+        emailEl.innerHTML = user.email;
+        avatarEl.src = user.photoURL;
 
         // logout
-        const LogoutBtn =document.getElementById("Logout");
-        LogoutBtn.addEventListener("click",()=>{
+        const LogoutBtn = document.getElementById("Logout");
+        LogoutBtn.addEventListener("click", () => {
             localStorage.removeItem("user");
-            window.location.href="./login.html"
+            window.location.href = "./login.html"
         })
-            
+
 
     })
 
-   
-    
 
-}else{
+
+
+} else {
     window.location.href = "login.html";
 }
